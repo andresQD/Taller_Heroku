31e41e9297b91282b8a8832e5c2e377753f20db0
Andres Quintero <andres.quintero98@hotmail.com>
2020-08-20 20:43:47 -0500

    pr2

--- src/test/java/edu/escuelaing/arep/AppTest.java
+++ src/test/java/edu/escuelaing/arep/AppTest.java
@@ -3,6 +3,8 @@ package edu.escuelaing.arep;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
@@ -27,36 +29,18 @@ public class AppTest {
 
         assertTrue(true);
     }
+    
+     @Test
+    public void pruebasJUnit() {
 
-    @Test
-    public void promedioDeberiaSerCorrecto() throws IOException {
-        LinkedList lista = new LinkedList();
-        BufferedReader br = new BufferedReader(new FileReader("src\\main\\resources\\file.txt"));
-        String str = null;
-        while ((str = br.readLine()) != null) {
-            float valor = Float.parseFloat(str);
-            lista.insertarInicio((float) valor);;
-        }
-        double promedio = App.promedio(lista);
-        assertEquals(String.format("%.2f", promedio), "638,90");
-        br.close();
+        assertTrue(true);
+    }
+    
+     @Test
+    public void pruebarJUnit() {
+
+        assertTrue(true);
     }
 
-    @Test
-    public void desviacionDeberiaSerCorrecta() throws IOException {
-        LinkedList lista = new LinkedList();
-        LinkedList lista1 = new LinkedList();
-        BufferedReader br = new BufferedReader(new FileReader("src\\main\\resources\\file.txt"));
-        String str = null;
-        while ((str = br.readLine()) != null) {
-            float valor = Float.parseFloat(str);
-            lista.insertarInicio(valor);
-            lista1.insertarInicio(valor);
-        }
-        float promedio = App.promedio(lista);
-        float desviacion = App.desviacion(lista1, promedio);
-        //System.out.println(desviacion);
-        assertEquals(String.format("%.2f", desviacion), "625,63");
-        br.close();
-    }
+
 }
